384df9aace617fbf85b9c4c82e4093139707b3f0
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-09-11 19:11:03 -0400

    refactored routes and added views

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const express = require('express');
 const app     = express();
 const PORT    =  process.env.PORT || 3000;
 
+var hbs = require('hbs');
+
+app.set("view engine", "hbs");
+
 app.get('/', (req, res) => {
-    res.send('Welcome to Pizza Express!')
+    res.render('index', {message: 'Welcome to Pizza Express!'})
 })
 
-app.get('/topping/:type', function(req, res, next) {
-    res.send(`${req.params.type} pizza! Good choice!`);
+app.get('/toppings/:type', function(req, res, next) {
+    res.render('toppings', {x: req.params.type});
 });
 
 app.get('/order/:amount/:size', function(req, res, next) {
-    res.send(`Your order of ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
-});
+    res.render('order',  {y: req.params.amount, z: req.params.size})
 
-
+})
 
 
 app.listen(PORT, function(){
@@ -23,4 +26,3 @@ app.listen(PORT, function(){
   console.log(`LISTENING ON PORT ${PORT}`);
   console.log("==========================")
 });
-
