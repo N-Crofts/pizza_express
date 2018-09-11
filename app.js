9af17cfc3ee037349c68b1281a3ab85a182259ae
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-09-11 18:11:14 -0400

    set up hbs views

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-// app.js
 
-//require express package
 const express = require('express');
-//save an express module as 'app'
 const app     = express();
-// assigning 3000 as our port
 const PORT    =  process.env.PORT || 3000;
 
-// tells the server to listen for requests on port 3000
+app.get('/', (req, res) => {
+    res.send('Welcome to Pizza Express!')
+})
+
+app.get('/topping/:type', function(req, res, next) {
+    res.send(`${req.params.type} pizza! Good choice!`);
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+    res.send(`Your order of ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+});
+
+
+
+
 app.listen(PORT, function(){
   console.log("==========================")
   console.log(`LISTENING ON PORT ${PORT}`);
   console.log("==========================")
 });
+
